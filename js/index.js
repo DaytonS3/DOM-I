0c4a3de146119415175b4ed99f33193b6c9e8720
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-03-04 17:30:30 -0500

    used appendchild / prepend

--- js/index.js
+++ js/index.js
@@ -48,6 +48,8 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 //NAVIGATION
 
+//AppendChild
+
 let NavA = document.querySelectorAll('a');
 
 
@@ -60,12 +62,20 @@ NavA[4].textContent = siteContent.nav["nav-item-5"]
 NavA[5].textContent = siteContent.nav["nav-item-6"]
 
 
+let x = document.createElement('a');
+let t = document.createTextNode("Nav item-1");
+x.appendChild(t);
+document.querySelector('nav').appendChild(x);
+
+let x2 = document.createElement('a');
+let t2 = document.createTextNode("Nav item-2");
+x.prepend(t2);
+document.querySelector('nav').prepend(x2);
+
 const navItems = document.querySelectorAll('a');
 navItems.forEach( function( currentValue ){
   currentValue.style.color = 'green';
 });
-
-
 //CALL TO ACTION
 
 //h1
@@ -171,36 +181,3 @@ contactPThree[7].textContent = siteContent["contact"]["email"];
 let footerP = document.querySelectorAll('p');
 
 footerP[8].textContent = siteContent["footer"]["copyright"];
-
-
-
-
-
-
-
-
-
-
-// "main-content": {
-//   "features-h4":"Features",
-//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "about-h4":"About",
-//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "middle-img-src": "img/mid-page-accent.jpg",
-//   "services-h4":"Services",
-//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "product-h4":"Product",
-//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "vision-h4":"Vision",
-//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-// },
-// "contact": {
-//   "contact-h4" : "Contact",
-//   "address" : "123 Way 456 Street Somewhere, USA",
-//   "phone" : "1 (888) 888-8888",
-//   "email" : "sales@greatidea.io",
-// },
-// "footer": {
-//   "copyright" : "Copyright Great Idea! 2018"
-// },
-// };
